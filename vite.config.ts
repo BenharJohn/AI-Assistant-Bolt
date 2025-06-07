ac1077954ca5f1b47281c57c0119c4d7fde0bff3
BenharJohn <76046876+BenharJohn@users.noreply.github.com>
2025-06-06 20:19:45 -0700

    Fix Vite configuration by removing Netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import netlify from '@netlify/vite-plugin' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-            netlify()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
