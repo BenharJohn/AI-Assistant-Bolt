3fa70da4a37226a9d49a0de8119d18ddd66eefd1
BenharJohn <76046876+BenharJohn@users.noreply.github.com>
2025-06-26 09:16:49 -0700

    Fix Vite config to conditionally load Netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import netlify from '@netlify/vite-plugin' 
+import netlify from '@netlify/vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),
-            netlify()],
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    ...(command === 'build' ? [netlify()] : [])
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
