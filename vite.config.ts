c100768178ab72b91168caba316cea9acd2958d7
BenharJohn <76046876+BenharJohn@users.noreply.github.com>
2025-06-27 16:50:14 -0700

    Fix Netlify Edge Functions Vite development errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import netlify from '@netlify/vite-plugin-netlify';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [
-    react(),
-    netlify()
+    react()
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
