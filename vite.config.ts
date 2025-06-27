ae9aaa9fe50c8e02e4956204dff1837b28c5b52b
BenharJohn <76046876+BenharJohn@users.noreply.github.com>
2025-06-27 16:47:30 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import netlify from '@netlify/vite-plugin-netlify';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [
-    react()
+    react(),
+    netlify()
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
