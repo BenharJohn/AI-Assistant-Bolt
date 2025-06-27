f11ebe322838544a202b9df44798a88aafe3944e
BenharJohn <76046876+BenharJohn@users.noreply.github.com>
2025-06-27 16:42:52 -0700

    Fix Netlify API proxy for development

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import netlify from '@netlify/vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [
     react(),
-    ...(command === 'build' ? [netlify()] : [])
+    netlify()
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})); 
+}));
