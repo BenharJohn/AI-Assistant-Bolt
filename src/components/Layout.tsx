200608d089fc1a6fb2b617db3c72618f52ec8867
BenharJohn <76046876+BenharJohn@users.noreply.github.com>
2025-06-28 20:08:44 -0700

    Make Fox Logo Bigger and Closer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -47,12 +47,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen bg-background text-foreground">
       {/* Mobile Header */}
       <header className="lg:hidden flex items-center justify-between p-4 bg-card border-b">
-        <div className="flex items-center space-x-3">
-          <div className="w-12 h-12 flex items-center justify-center">
-            <img src={foxIcon} alt="Aeva Fox" className="w-11 h-11 object-contain" />
+        <div className="flex items-center space-x-1">
+          <div className="w-14 h-14 flex items-center justify-center">
+            <img src={foxIcon} alt="Aeva Fox" className="w-13 h-13 object-contain" />
           </div>
-          <div className="h-8 flex items-center">
-            <img src={aevaLogo} alt="Aeva" className="h-7 object-contain" />
+          <div className="h-9 flex items-center">
+            <img src={aevaLogo} alt="Aeva" className="h-8 object-contain" />
           </div>
         </div>
         <div className="flex items-center space-x-2">
@@ -99,12 +99,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <aside className="fixed h-screen w-64 bg-card border-r">
           <div className="flex flex-col h-full">
             <div className="p-6">
-              <div className="flex items-center space-x-2">
-                <div className="w-16 h-16 flex items-center justify-center">
-                  <img src={foxIcon} alt="Aeva Fox" className="w-15 h-15 object-contain" />
+              <div className="flex items-center space-x-1">
+                <div className="w-20 h-20 flex items-center justify-center">
+                  <img src={foxIcon} alt="Aeva Fox" className="w-18 h-18 object-contain" />
                 </div>
-                <div className="h-12 flex items-center">
-                  <img src={aevaLogo} alt="Aeva" className="h-10 object-contain" />
+                <div className="h-14 flex items-center">
+                  <img src={aevaLogo} alt="Aeva" className="h-12 object-contain" />
                 </div>
               </div>
             </div>
